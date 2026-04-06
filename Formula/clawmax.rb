d8b52e2f1593b4bfc55585f08cc0358c6bb27ea5
dr.max <mmaximilien@gmail.com>
2026-04-06 14:06:44 +0000

    update clawmax formula to v1.3.7

--- Formula/clawmax.rb
+++ Formula/clawmax.rb
@@ -7,20 +7,20 @@ class Clawmax < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.3.7/clawmax-v1.3.7-darwin-arm64.tar.gz"
-      sha256 "0683b783dc746db12e2623fb74983134acbd741f2b1c9b74e88c769263fb4198"
+      sha256 "82ee27091e977bbd4e8b6e8b0750012f6baa794f5c22646ceee06dc0f3a3f73c"
     else
       url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.3.7/clawmax-v1.3.7-darwin-amd64.tar.gz"
-      sha256 "4ee27dec664a850aa0031efddb1fa255a3b060b252259a13ddb27d68835c057a"
+      sha256 "9209466c4e8365f3c484f5f9ffcc06643c006f3636fde1ea82800150f243e836"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.3.7/clawmax-v1.3.7-linux-arm64.tar.gz"
-      sha256 "620017eab88edd78af9ac8b148ac510d913112d4da595c9205beedae675ff520"
+      sha256 "f87cb4e628c042c0a388dd17ce682cec3a30a0f40808332cb7e37bf8ab3a68aa"
     else
       url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.3.7/clawmax-v1.3.7-linux-amd64.tar.gz"
-      sha256 "b51e80709cc920ca4a1ffeb49cb2f09e53a254810cf9c348869e64bc9c87823c"
+      sha256 "6f17d03187a12b546671683e5253bc37b7aa22ec5f0a7562ecb6579b0f86fffc"
     end
   end
 
