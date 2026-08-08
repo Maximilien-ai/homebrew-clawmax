e7cedd3ca685ec0e0f3171292a5ae61f5b3902dd
dr.max <mmaximilien@gmail.com>
2026-08-08 06:40:55 -0700

    update clawmax-app cask to v1.9.22

--- Casks/clawmax-app.rb
+++ Casks/clawmax-app.rb
@@ -1,13 +1,13 @@
 cask "clawmax-app" do
-  version "1.9.21"
-  sha256 "3dacba6edd47b16ec186c6fdedf9e418cf30c7089b5330a40ed67b3c6dc3bcfc"
+  version "1.9.22"
+  sha256 "2bb9a2c9c75029b6d8813b522a123da16ae07b56ff0f7ef3aceff4024218f96b"
 
-  url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.9.21/ClawMax-1.9.21.pkg"
+  url "https://github.com/Maximilien-ai/clawmax-cli-releases/releases/download/v1.9.22/ClawMax-1.9.22.pkg"
   name "ClawMax"
   desc "ClawMax Agent and App for on-premise deployments"
   homepage "https://clawmax.ai"
 
-  pkg "ClawMax-1.9.21.pkg"
+  pkg "ClawMax-1.9.22.pkg"
 
   uninstall pkgutil: "ai.maximilien.clawmax"
 end
